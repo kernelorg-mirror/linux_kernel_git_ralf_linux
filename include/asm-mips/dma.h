4c0c3ea57584fef7404172baf3a6928977d001f9
Ralf Baechle <ralf@linux-mips.org>
2000-10-05 11:56:02 +0000

    Define isa_dma_bridge_buggy.  Patch from Spock.

--- include/asm-mips/dma.h
+++ include/asm-mips/dma.h
@@ -299,4 +299,10 @@ static __inline__ int get_dma_residue(unsigned int dmanr)
 extern int request_dma(unsigned int dmanr, const char * device_id);	/* reserve a DMA channel */
 extern void free_dma(unsigned int dmanr);	/* release it again */
 
+#ifdef CONFIG_PCI_QUIRKS
+extern int isa_dma_bridge_buggy;
+#else
+#define isa_dma_bridge_buggy    (0)
+#endif
+
 #endif /* __ASM_MIPS_DMA_H */
