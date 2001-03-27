3d58b36595f64ce29a60fd61fc133b18d74d7ede
Ralf Baechle <ralf@linux-mips.org>
2001-03-27 01:24:21 +0000

    Interpret some ARC firmware variables correctly.  Patch from Guido with some mods by me.

--- arch/mips/arc/cmdline.c
+++ arch/mips/arc/cmdline.c
@@ -16,7 +16,7 @@
 
 char arcs_cmdline[CL_SIZE];
 
-__initfunc(char *prom_getcmdline(void))
+char * __init prom_getcmdline(void)
 {
 	return &(arcs_cmdline[0]);
 }
@@ -27,11 +27,49 @@ static char *ignored[] = {
 	"SystemPartition=",
 	"OSLoader=",
 	"OSLoadPartition=",
-	"OSLoadFilename="
+	"OSLoadFilename=",
+	"OSLoadOptions="
 };
 #define NENTS(foo) ((sizeof((foo)) / (sizeof((foo[0])))))
 
-__initfunc(void prom_init_cmdline(void))
+static char *used_arc[][2] = {
+	{ "OSLoadPartition=", "root=" },
+	{ "OSLoadOptions=", "" }
+};
+
+static char * __init move_firmware_args(char* cp)
+{
+	char *s;
+	int actr, i;
+
+	actr = 1; /* Always ignore argv[0] */
+
+	while (actr < prom_argc) {
+		for(i = 0; i < NENTS(used_arc); i++) {
+			int len = strlen(used_arc[i][0]);
+
+			if(!strncmp(prom_argv[actr], used_arc[i][0], len)) {
+			/* Ok, we want it. First append the replacement... */
+				strcat(cp, used_arc[i][1]);
+				cp += strlen(used_arc[i][1]);
+				/* ... and now the argument */
+				s = strstr(prom_argv[actr], "=");
+				if(s) {
+					s++;
+					strcpy(cp, s);
+					cp += strlen(s);
+				}
+				*cp++ = ' ';
+				break;
+			}
+		}
+		actr++;
+	}
+
+	return cp;
+}
+
+void __init prom_init_cmdline(void)
 {
 	char *cp;
 	int actr, i;
@@ -39,8 +77,14 @@ __initfunc(void prom_init_cmdline(void))
 	actr = 1; /* Always ignore argv[0] */
 
 	cp = &(arcs_cmdline[0]);
-	while(actr < prom_argc) {
-		for(i = 0; i < NENTS(ignored); i++) {
+	/* 
+	 * Move ARC variables to the beginning to make sure they can be
+	 * overridden by later arguments.
+	 */
+	cp = move_firmware_args(cp);
+
+	while (actr < prom_argc) {
+		for (i = 0; i < NENTS(ignored); i++) {
 			int len = strlen(ignored[i]);
 
 			if(!strncmp(prom_argv[actr], ignored[i], len))
