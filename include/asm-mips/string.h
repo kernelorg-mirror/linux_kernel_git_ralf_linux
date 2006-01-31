091ec9a539520f84cafde322c13ee8ddc26a4c59
Ralf Baechle <ralf@linux-mips.org>
2006-01-31 17:13:26 +0000

    Remove buggy inline version of memscan.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/string.h
+++ include/asm-mips/string.h
@@ -129,24 +129,4 @@ extern void *memmove(void *__dest, __const__ void *__src, size_t __n);
 /* Don't build bcopy at all ...  */
 #define __HAVE_ARCH_BCOPY
 
-#define __HAVE_ARCH_MEMSCAN
-extern __inline__ void *memscan(void *__addr, int __c, size_t __size)
-{
-	char *__end = (char *)__addr + __size;
-
-	__asm__(".set\tpush\n\t"
-		".set\tnoat\n\t"
-		".set\treorder\n\t"
-		"1:\tbeq\t%0,%1,2f\n\t"
-		"addiu\t%0,1\n\t"
-		"lb\t$1,-1(%0)\n\t"
-		"bne\t$1,%4,1b\n"
-		"2:\t.set\tpop"
-		: "=r" (__addr), "=r" (__end)
-		: "0" (__addr), "1" (__end), "r" (__c)
-		: "$1");
-
-	return __addr;
-}
-
 #endif /* _ASM_STRING_H */
