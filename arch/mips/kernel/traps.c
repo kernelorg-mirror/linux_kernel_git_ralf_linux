67106b5d94de4e490684847da02366d34274f885
Ralf Baechle <ralf@linux-mips.org>
2008-04-20 16:28:54 +0100

    [MIPS] Fix handling of trap and breakpoint instructions
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit 5881bb0de64887a60f7f49922cf73a3b4d40fc01)

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -662,8 +662,8 @@ asmlinkage void do_bp(struct pt_regs *regs)
 	 * We handle both cases with a simple heuristics.  --macro
 	 */
 	bcode = ((opcode >> 6) & ((1 << 20) - 1));
-	if (bcode < (1 << 10))
-		bcode <<= 10;
+	if (bcode >= (1 << 10))
+		bcode >>= 10;
 
 	/*
 	 * (A short test says that IRIX 5.3 sends SIGTRAP for all break
@@ -672,8 +672,8 @@ asmlinkage void do_bp(struct pt_regs *regs)
 	 * But should we continue the brokenness???  --macro
 	 */
 	switch (bcode) {
-	case BRK_OVERFLOW << 10:
-	case BRK_DIVZERO << 10:
+	case BRK_OVERFLOW:
+	case BRK_DIVZERO:
 		die_if_kernel("Break instruction in kernel code", regs);
 		if (bcode == (BRK_DIVZERO << 10))
 			info.si_code = FPE_INTDIV;
@@ -685,7 +685,8 @@ asmlinkage void do_bp(struct pt_regs *regs)
 		force_sig_info(SIGFPE, &info, current);
 		break;
 	case BRK_BUG:
-		die("Kernel bug detected", regs);
+		die_if_kernel("Kernel bug detected", regs);
+		force_sig(SIGTRAP, current);
 		break;
 	default:
 		die_if_kernel("Break instruction in kernel code", regs);
@@ -729,7 +730,8 @@ asmlinkage void do_tr(struct pt_regs *regs)
 		force_sig_info(SIGFPE, &info, current);
 		break;
 	case BRK_BUG:
-		die("Kernel bug detected", regs);
+		die_if_kernel("Kernel bug detected", regs);
+		force_sig(SIGTRAP, current);
 		break;
 	default:
 		die_if_kernel("Trap instruction in kernel code", regs);
