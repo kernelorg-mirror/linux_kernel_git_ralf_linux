a23abcb2d6d967d8e715265f9d86b152d70c649f
Ralf Baechle <ralf@linux-mips.org>
2008-07-03 23:28:35 +0100

    [MIPS] Fix bug in atomic_sub_if_positive.
    The branch optimization fixes in 2.6.21 introduced a bug in
    atomic_sub_if_positive that causes it to return even when the sc
    instruction fails. The result is that e.g. down_trylock becomes unreliable
    as the semaphore counter is not always decremented.
    
    Original MUA-shredded patch from Morten Larsen <mlarsen@broadcom.com>.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit 38f3573ce360a5b8f4f57b815b903f22e1efacdb)

--- include/asm-mips/atomic.h
+++ include/asm-mips/atomic.h
@@ -282,10 +282,10 @@ static __inline__ int atomic_sub_if_positive(int i, atomic_t * v)
 		"	beqz	%0, 2f					\n"
 		"	 subu	%0, %1, %3				\n"
 		"	.set	reorder					\n"
-		"1:							\n"
 		"	.subsection 2					\n"
 		"2:	b	1b					\n"
 		"	.previous					\n"
+		"1:							\n"
 		"	.set	mips0					\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
 		: "Ir" (i), "m" (v->counter)
@@ -657,10 +657,10 @@ static __inline__ long atomic64_sub_if_positive(long i, atomic64_t * v)
 		"	beqz	%0, 2f					\n"
 		"	 dsubu	%0, %1, %3				\n"
 		"	.set	reorder					\n"
-		"1:							\n"
 		"	.subsection 2					\n"
 		"2:	b	1b					\n"
 		"	.previous					\n"
+		"1:							\n"
 		"	.set	mips0					\n"
 		: "=&r" (result), "=&r" (temp), "=m" (v->counter)
 		: "Ir" (i), "m" (v->counter)
