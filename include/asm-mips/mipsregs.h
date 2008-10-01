9a26ad2afb229aac834f343df0ae6d68c5cdea13
Ralf Baechle <ralf@linux-mips.org>
2008-10-01 21:52:41 +0100

    [MIPS] Build fix: Fix irq flags type
    Though from a hardware perspective it would be sensible to use only a
    32-bit unsigned int type Linux defines interrupt flags to be stored in
    an unsigned long and nothing else.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit cd1e9a63ee14f7e4ad0f994821b3c58326f86fde)

--- include/asm-mips/mipsregs.h
+++ include/asm-mips/mipsregs.h
@@ -1426,7 +1426,7 @@ set_c0_##name(unsigned int set)					\
 {								\
 	unsigned int res;					\
 	unsigned int omt;					\
-	unsigned int flags;					\
+	unsigned long flags;					\
 								\
 	local_irq_save(flags);					\
 	omt = __dmt();						\
@@ -1444,7 +1444,7 @@ clear_c0_##name(unsigned int clear)				\
 {								\
 	unsigned int res;					\
 	unsigned int omt;					\
-	unsigned int flags;					\
+	unsigned long flags;					\
 								\
 	local_irq_save(flags);					\
 	omt = __dmt();						\
@@ -1462,7 +1462,7 @@ change_c0_##name(unsigned int change, unsigned int new)		\
 {								\
 	unsigned int res;					\
 	unsigned int omt;					\
-	unsigned int flags;					\
+	unsigned long flags;					\
 								\
 	local_irq_save(flags);					\
 								\
