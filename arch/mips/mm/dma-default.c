07fcb7c461277a6fe36dd558d61f0b9200335609
David Daney <ddaney@caviumnetworks.com>
2008-10-17 17:48:53 -0700

    MIPS: Don't unmap the memory for dma_sync*.
    We were getting away with this for so long only because the only platform
    with a non-empty plat_unmap_dma_mem() doesn't call dma_sync_sg_for_cpu()
    and dma_sync_sg_for_device() from its commonly used drivers.
    
    Signed-off-by: David Daney <ddaney@caviumnetworks.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit cab18e34253ef83119d9883dd036930fd01959d4)

--- arch/mips/mm/dma-default.c
+++ arch/mips/mm/dma-default.c
@@ -306,7 +306,6 @@ void dma_sync_sg_for_cpu(struct device *dev, struct scatterlist *sg, int nelems,
 		if (cpu_is_noncoherent_r10000(dev))
 			__dma_sync((unsigned long)page_address(sg->page),
 			           sg->length, direction);
-		plat_unmap_dma_mem(sg->dma_address);
 	}
 }
 
@@ -324,7 +323,6 @@ void dma_sync_sg_for_device(struct device *dev, struct scatterlist *sg, int nele
 		if (!plat_device_is_coherent(dev))
 			__dma_sync((unsigned long)page_address(sg->page),
 			           sg->length, direction);
-		plat_unmap_dma_mem(sg->dma_address);
 	}
 }
 
