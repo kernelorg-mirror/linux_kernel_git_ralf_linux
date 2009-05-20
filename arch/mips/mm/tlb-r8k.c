45a482ff7851bae9006829f9241aa05ac53afa67
Greg Ungerer <gerg@snapgear.com>
2009-05-20 16:12:32 +1000

    MIPS: 64-bit: Fix system lockup with 2.6.29 on Cavium/Octeon
    The address range size calculation inside local_flush_tlb_kernel_range()
    is being truncated by a too small size variable holder on 64-bit systems.
    The truncated size can result in an erroneous tlbsize check that means we
    sit spinning inside a loop trying to flush a hige number of TLB entries.
    This is for all intents and purposes a system hang. Fix by using an
    appropriately sized valiable to hold the size.
    
    [Ralf: Greg's original patch submission identified the issue and fixed one
    instance in tlb-r4k.c but there there were several more.  For consistency
    I also modified tlb-r3k.c even though that file is only used on 32-bit.]
    
    Signed-off-by: Greg Ungerer <gerg@snapgear.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit c5aac5261cf1edf8bd143958762786b044832f26)

--- arch/mips/mm/tlb-r8k.c
+++ arch/mips/mm/tlb-r8k.c
@@ -112,8 +112,7 @@ out_restore:
 /* Usable for KV1 addresses only! */
 void local_flush_tlb_kernel_range(unsigned long start, unsigned long end)
 {
-	unsigned long flags;
-	int size;
+	unsigned long size, flags;
 
 	size = (end - start + (PAGE_SIZE - 1)) >> PAGE_SHIFT;
 	size = (size + 1) >> 1;
