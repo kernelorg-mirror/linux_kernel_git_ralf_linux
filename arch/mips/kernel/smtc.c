0b27c702f8ee455dfa9d5a6724420d9d2379296c
Kurt Martin <kurt@mips.com>
2009-07-08 19:22:35 -0700

    MIPS: SMTC: Move cross VPE writes to after a TC is assigned to VPE.
    Signed-off-by: Chris Dearman <chris@mips.com>
    Signed-off-by: Raghu Gandham <raghu@mips.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit 4eebdebd71325d0814b1c8e093fd0d1f191da9aa)

--- arch/mips/kernel/smtc.c
+++ arch/mips/kernel/smtc.c
@@ -405,11 +405,8 @@ void mipsmt_prepare_cpus(void)
 	smtc_configure_tlb();
 
 	for (tc = 0, vpe = 0 ; (vpe < nvpe) && (tc < ntc) ; vpe++) {
-		/*
-		 * Set the MVP bits.
-		 */
-		settc(tc);
-		write_vpe_c0_vpeconf0(read_vpe_c0_vpeconf0() | VPECONF0_MVP);
+		if (tcpervpe[vpe] == 0)
+			continue;
 		if (vpe != 0)
 			printk(", ");
 		printk("VPE %d: TC", vpe);
@@ -436,6 +433,12 @@ void mipsmt_prepare_cpus(void)
 			slop--;
 		}
 		if (vpe != 0) {
+			/*
+			 * Allow this VPE to control others.
+			 */
+			write_vpe_c0_vpeconf0(read_vpe_c0_vpeconf0() |
+					      VPECONF0_MVP);
+
 			/*
 			 * Clear any stale software interrupts from VPE's Cause
 			 */
