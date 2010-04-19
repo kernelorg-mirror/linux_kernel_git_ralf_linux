583730838bd8a841b47b82b38a6a8e9d1eca727b
David Daney <ddaney@caviumnetworks.com>
2010-04-19 11:43:10 -0700

    MIPS: Don't vmap things at address zero.
    In the 64-bit kernel we use swapper_pg_dir for three different things.
    
    1) xuseg mappings for kernel threads.
    
    2) vmap mappings for all kernel-space accesses in xkseg.
    
    3) vmap mappings for kernel modules in ksseg (kseg2).
    
    Due to how the TLB refill handlers work, any mapping established in
    xkseg or ksseg will also establish a xuseg mapping that should never
    be used by the kernel.
    
    In order to be able to use exceptions to trap NULL pointer
    dereferences, we need to ensure that nothing is mapped at address
    zero.  Since vmap mappings in xkseg are reflected in xuseg, this means
    we need to ensure that there are no vmap mappings established at the
    start of xkseg.  So we move back VMALLOC_START to avoid establishing
    vmap mappings at the start of xkseg.
    
    Signed-off-by: David Daney <ddaney@caviumnetworks.com>
    To: linux-mips@linux-mips.org
    Patchwork: http://patchwork.linux-mips.org/patch/1129/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit ca6d5db7d6b9c9f1789eca0a409276aa287a0646)

--- include/asm-mips/pgtable-64.h
+++ include/asm-mips/pgtable-64.h
@@ -97,9 +97,14 @@
 #define USER_PTRS_PER_PGD	(TASK_SIZE / PGDIR_SIZE)
 #define FIRST_USER_ADDRESS	0
 
-#define VMALLOC_START		MAP_BASE
+/*
+ * TLB refill handlers also map the vmalloc area into xuseg.  Avoid
+ * the first couple of pages so NULL pointer dereferences will still
+ * reliably trap.
+ */
+#define VMALLOC_START		(MAP_BASE + (2 * PAGE_SIZE))
 #define VMALLOC_END	\
-	(VMALLOC_START + PTRS_PER_PGD * PTRS_PER_PMD * PTRS_PER_PTE * PAGE_SIZE)
+	(MAP_BASE + PTRS_PER_PGD * PTRS_PER_PMD * PTRS_PER_PTE * PAGE_SIZE)
 
 #define pte_ERROR(e) \
 	printk("%s:%d: bad pte %016lx.\n", __FILE__, __LINE__, pte_val(e))
