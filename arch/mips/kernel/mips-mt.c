c92aa6af243f598b6b6e09c2135c02d49215cbc6
Ralf Baechle <ralf@linux-mips.org>
2010-05-29 03:19:57 +0100

    MIPS: MT: Fix FPU affinity.
    The fragile MT sys_sched_setaffinity wrapper needs its regular dose of
    fixes.
    
    Nose-poked-at-pile-o-crap-by: Julia Lawall <julia@diku.dk>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit 73816db922ff32a05a3944e9314c8df881556670)

--- arch/mips/kernel/mips-mt.c
+++ arch/mips/kernel/mips-mt.c
@@ -12,6 +12,7 @@
 #include <linux/security.h>
 
 #include <asm/cpu.h>
+#include <linux/cpuset.h>
 #include <asm/processor.h>
 #include <asm/atomic.h>
 #include <asm/system.h>
@@ -55,17 +56,15 @@ static inline struct task_struct *find_process_by_pid(pid_t pid)
 	return pid ? find_task_by_pid(pid) : current;
 }
 
-
 /*
  * mipsmt_sys_sched_setaffinity - set the cpu affinity of a process
  */
 asmlinkage long mipsmt_sys_sched_setaffinity(pid_t pid, unsigned int len,
 				      unsigned long __user *user_mask_ptr)
 {
-	cpumask_t new_mask;
-	cpumask_t effective_mask;
-	int retval;
+	cpumask_t new_mask, effective_mask;
 	struct task_struct *p;
+	int retval;
 
 	if (len < sizeof(new_mask))
 		return -EINVAL;
@@ -90,13 +89,12 @@ asmlinkage long mipsmt_sys_sched_setaffinity(pid_t pid, unsigned int len,
 	 * set_cpus_allowed.
 	 */
 	get_task_struct(p);
+	read_unlock(&tasklist_lock);
 
 	retval = -EPERM;
 	if ((current->euid != p->euid) && (current->euid != p->uid) &&
-			!capable(CAP_SYS_NICE)) {
-		read_unlock(&tasklist_lock);
+			!capable(CAP_SYS_NICE))
 		goto out_unlock;
-	}
 
 	retval = security_task_setscheduler(p, 0, NULL);
 	if (retval)
@@ -105,15 +103,13 @@ asmlinkage long mipsmt_sys_sched_setaffinity(pid_t pid, unsigned int len,
 	/* Record new user-specified CPU set for future reference */
 	p->thread.user_cpus_allowed = new_mask;
 
-	/* Unlock the task list */
-	read_unlock(&tasklist_lock);
-
 	/* Compute new global allowed CPU set if necessary */
 	if( (p->thread.mflags & MF_FPUBOUND)
 	&& cpus_intersects(new_mask, mt_fpu_cpumask)) {
 		cpus_and(effective_mask, new_mask, mt_fpu_cpumask);
 		retval = set_cpus_allowed(p, effective_mask);
 	} else {
+		effective_mask = new_mask;
 		p->thread.mflags &= ~MF_FPUBOUND;
 		retval = set_cpus_allowed(p, new_mask);
 	}
