573bb1281fb472cecf841e3ac36a01a398debd66
Ralf Baechle <ralf@linux-mips.org>
2010-09-19 00:09:09 +0100

    MIPS: PNX8550: Sort out machine halt, restart and powerdown functions.
    No rubbish printks - those belong to userspace.  The halt function now
    actually halts the system and the poweroff function was deleted because
    it didn't actually power down the system.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit 99035278c311dc43ec859708d9b1d6ef1751ed7a)

--- arch/mips/philips/pnx8550/common/setup.c
+++ arch/mips/philips/pnx8550/common/setup.c
@@ -46,7 +46,6 @@ extern void prom_printf(char *fmt, ...);
 extern void __init board_setup(void);
 extern void pnx8550_machine_restart(char *);
 extern void pnx8550_machine_halt(void);
-extern void pnx8550_machine_power_off(void);
 extern struct resource ioport_resource;
 extern struct resource iomem_resource;
 extern void pnx8550_time_init(void);
@@ -105,7 +104,7 @@ void __init plat_mem_setup(void)
 
         _machine_restart = pnx8550_machine_restart;
         _machine_halt = pnx8550_machine_halt;
-        pm_power_off = pnx8550_machine_power_off;
+        pm_power_off = pnx8550_machine_halt;
 
 	board_time_init = pnx8550_time_init;
 
