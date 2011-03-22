2d5ef483d91edac191f29c5d4986c0c9d7f82442
David Daney <ddaney@caviumnetworks.com>
2011-03-22 16:34:48 -0700

    binfmt_elf: quiet GCC-4.6 'set but not used' warning in load_elf_binary()
    With GCC-4.6 we get warnings about things being 'set but not used'.
    
    In load_elf_binary() this can happen with reloc_func_desc if ELF_PLAT_INIT
    is defined, but doesn't use the reloc_func_desc argument.
    
    Quiet the warning/error by marking reloc_func_desc as __maybe_unused.
    
    Signed-off-by: David Daney <ddaney@caviumnetworks.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 1a530a6f23f7dca336311ef60c9ca26f3dc63688)

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -552,7 +552,7 @@ static int load_elf_binary(struct linux_binprm *bprm, struct pt_regs *regs)
 	unsigned int size;
 	unsigned long elf_entry, interp_load_addr = 0;
 	unsigned long start_code, end_code, start_data, end_data;
-	unsigned long reloc_func_desc = 0;
+	unsigned long reloc_func_desc __maybe_unused = 0;
 	char passed_fileno[6];
 	struct files_struct *files;
 	int executable_stack = EXSTACK_DEFAULT;
