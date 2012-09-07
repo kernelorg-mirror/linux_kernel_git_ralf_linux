cfaa83e49c0bf3a5b8b2600807913c654aea4e62
Ralf Baechle <ralf@linux-mips.org>
2012-09-07 09:34:30 +0200

    Revert "MIPS: add support for software performance events"
    This reverts commit 7f788d2d53085815d474559cd51ef1f38b0a9bb8.
    
    This just doesn't build in 2.6.37.  If you need this feature upgrade.

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -30,7 +30,6 @@
 #include <linux/notifier.h>
 #include <linux/kdb.h>
 #include <linux/irq.h>
-#include <linux/perf_event.h>
 
 #include <asm/bootinfo.h>
 #include <asm/branch.h>
@@ -587,16 +586,10 @@ static inline int simulate_sc(struct pt_regs *regs, unsigned int opcode)
  */
 static int simulate_llsc(struct pt_regs *regs, unsigned int opcode)
 {
-	if ((opcode & OPCODE) == LL) {
-		perf_sw_event(PERF_COUNT_SW_EMULATION_FAULTS,
-				1, 0, regs, 0);
+	if ((opcode & OPCODE) == LL)
 		return simulate_ll(regs, opcode);
-	}
-	if ((opcode & OPCODE) == SC) {
-		perf_sw_event(PERF_COUNT_SW_EMULATION_FAULTS,
-				1, 0, regs, 0);
+	if ((opcode & OPCODE) == SC)
 		return simulate_sc(regs, opcode);
-	}
 
 	return -1;			/* Must be something else ... */
 }
@@ -612,8 +605,6 @@ static int simulate_rdhwr(struct pt_regs *regs, unsigned int opcode)
 	if ((opcode & OPCODE) == SPEC3 && (opcode & FUNC) == RDHWR) {
 		int rd = (opcode & RD) >> 11;
 		int rt = (opcode & RT) >> 16;
-		perf_sw_event(PERF_COUNT_SW_EMULATION_FAULTS,
-				1, 0, regs, 0);
 		switch (rd) {
 		case 0:		/* CPU number */
 			regs->regs[rt] = smp_processor_id();
@@ -649,11 +640,8 @@ static int simulate_rdhwr(struct pt_regs *regs, unsigned int opcode)
 
 static int simulate_sync(struct pt_regs *regs, unsigned int opcode)
 {
-	if ((opcode & OPCODE) == SPEC0 && (opcode & FUNC) == SYNC) {
-		perf_sw_event(PERF_COUNT_SW_EMULATION_FAULTS,
-				1, 0, regs, 0);
+	if ((opcode & OPCODE) == SPEC0 && (opcode & FUNC) == SYNC)
 		return 0;
-	}
 
 	return -1;			/* Must be something else ... */
 }
