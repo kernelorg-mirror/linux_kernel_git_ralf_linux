0849c415a87448d44a5fcb2348b50092848670a6
Ralf Baechle <ralf@linux-mips.org>
2012-09-11 20:10:45 +0200

    MIPS: SMP: Fix build error due to warning.
      CC      arch/mips/kernel/smp.o
    /home/ralf/src/linux/linux-stable/arch/mips/kernel/smp.c: In function ‘smp_call_function_single’:
    /home/ralf/src/linux/linux-stable/arch/mips/kernel/smp.c:249:6: error: variable ‘ret’ set but not used [-Werror=unused-but-set-variable]
    cc1: all warnings being treated as errors
    
    Fixed by passing on the error code in ret.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/smp.c
+++ arch/mips/kernel/smp.c
@@ -269,7 +269,8 @@ int smp_call_function_single(int cpu, void (*func) (void *info), void *info,
 				     wait);
 
 	put_cpu();
-	return 0;
+
+	return ret;
 }
 EXPORT_SYMBOL(smp_call_function_single);
 
