ddd687ecda637d06ed03472278d1ddc5b242efc9
Ralf Baechle <ralf@linux-mips.org>
2014-11-18 18:47:13 +0100

    MIPS: Zero variable read by get_user / __get_user in case of an error.
    This wasn't happening in all cases.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit 6f3bd9987cb56012bf1fce1b067844ff7cf8a9e6)

--- include/asm-mips/uaccess.h
+++ include/asm-mips/uaccess.h
@@ -247,6 +247,8 @@ do {									\
 									\
 	if (likely(access_ok(VERIFY_READ,  __gu_ptr, size)))		\
 		__get_user_common((x), size, __gu_ptr);			\
+	else								\
+		(x) = 0;						\
 									\
 	__gu_err;							\
 })
@@ -260,6 +262,7 @@ do {									\
 	"2:							\n"	\
 	"	.section .fixup,\"ax\"				\n"	\
 	"3:	li	%0, %4					\n"	\
+	"	move	%1, $0					\n"	\
 	"	j	2b					\n"	\
 	"	.previous					\n"	\
 	"	.section __ex_table,\"a\"			\n"	\
