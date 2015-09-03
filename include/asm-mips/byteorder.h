c63ec7230b48f14a0a79b2eb46392c5459766f1f
Yousong Zhou <yszhou4tech@gmail.com>
2015-09-03 17:47:45 +0800

    MIPS: UAPI: Fix unrecognized opcode WSBH/DSBH/DSHD when using MIPS16.
    The nomips16 has to be added both as function attribute and assembler
    directive.
    
    When only function attribute is specified, the compiler will inline the
    function with -Os optimization.  The generated assembly code cannot be
    correctly assembled because ISA mode switch has to be done through jump
    instruction.
    
    When only ".set nomips16" directive is used, the generated assembly code
    will use MIPS32 code for the inline assembly template and MIPS16 for the
    function return.  The compiled binary is invalid:
    
        00403100 <__arch_swab16>:
          403100:   7c0410a0    wsbh    v0,a0
          403104:   e820ea31    swc2    $0,-5583(at)
    
    while correct code should be:
    
        00402650 <__arch_swab16>:
          402650:   7c0410a0    wsbh    v0,a0
          402654:   03e00008    jr  ra
          402658:   3042ffff    andi    v0,v0,0xffff
    
    Signed-off-by: Yousong Zhou <yszhou4tech@gmail.com>
    Cc: Chen Jie <chenj@lemote.com>
    Cc: linux-mips@linux-mips.org
    Cc: linux-kernel@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/11087/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit 106c612ac609f72051fe6962c32cef80f49bbd27)

--- include/asm-mips/byteorder.h
+++ include/asm-mips/byteorder.h
@@ -16,12 +16,15 @@
 
 #ifdef CONFIG_CPU_MIPSR2
 
-static __inline__ __attribute_const__ __u16 ___arch__swab16(__u16 x)
+static __inline__ __attribute_const__ __attribute__((nomips16))
+	__u16 ___arch__swab16(__u16 x)
 {
 	__asm__(
-	"	.set	mips32r2		\n"
+	"	set	push			\n"
+	"	set	arch=mips32r2		\n"
+	"	set	nomips16		\n"
 	"	wsbh	%0, %1			\n"
-	"	.set	mips0			\n"
+	"	.set	pop			\n"
 	: "=r" (x)
 	: "r" (x));
 
@@ -29,13 +32,16 @@ static __inline__ __attribute_const__ __u16 ___arch__swab16(__u16 x)
 }
 #define __arch__swab16(x)	___arch__swab16(x)
 
-static __inline__ __attribute_const__ __u32 ___arch__swab32(__u32 x)
+static __inline__ __attribute_const__ __attribute__((nomips16))
+	__u32 ___arch__swab32(__u32 x)
 {
 	__asm__(
-	"	.set	mips32r2		\n"
+	"	set	push			\n"
+	"	set	arch=mips32r2		\n"
+	"	set	nomips16		\n"
 	"	wsbh	%0, %1			\n"
 	"	rotr	%0, %0, 16		\n"
-	"	.set	mips0			\n"
+	"	.set	pop			\n"
 	: "=r" (x)
 	: "r" (x));
 
