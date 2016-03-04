7259d1c8ee344dfeefb96021fb4614d17033e963
Maciej W. Rozycki <macro@imgtec.com>
2016-03-04 01:42:49 +0000

    MIPS: traps: Fix SIGFPE information leak from `do_ov' and `do_trap_or_bp'
    Avoid sending a partially initialised `siginfo_t' structure along SIGFPE
    signals issued from `do_ov' and `do_trap_or_bp', leading to information
    leaking from the kernel stack.
    
    Signed-off-by: Maciej W. Rozycki <macro@imgtec.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit a110d94012ee365ade7f3baab6b3b79aa019071d)

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -533,12 +533,12 @@ static inline int simulate_llsc(struct pt_regs *regs)
 
 asmlinkage void do_ov(struct pt_regs *regs)
 {
-	siginfo_t info;
+	siginfo_t info = {
+		.si_signo = SIGFPE,
+		.si_code = FPE_INTOVF,
+		.si_addr = (void __user *)regs->cp0_epc,
+	};
 
-	info.si_code = FPE_INTOVF;
-	info.si_signo = SIGFPE;
-	info.si_errno = 0;
-	info.si_addr = (void *)regs->cp0_epc;
 	force_sig_info(SIGFPE, &info, current);
 }
 
@@ -588,7 +588,7 @@ asmlinkage void do_fpe(struct pt_regs *regs, unsigned long fcr31)
 asmlinkage void do_bp(struct pt_regs *regs)
 {
 	unsigned int opcode, bcode;
-	siginfo_t info;
+	siginfo_t info = { 0 };
 
 	die_if_kernel("Break instruction in kernel code", regs);
 
@@ -619,7 +619,6 @@ asmlinkage void do_bp(struct pt_regs *regs)
 		else
 			info.si_code = FPE_INTOVF;
 		info.si_signo = SIGFPE;
-		info.si_errno = 0;
 		info.si_addr = (void *)regs->cp0_epc;
 		force_sig_info(SIGFPE, &info, current);
 		break;
