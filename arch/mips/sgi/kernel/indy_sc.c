f015cf882c709463182b6d98e31cd005aa379566
Ralf Baechle <ralf@linux-mips.org>
2016-12-15 12:27:21 +0100

    MIPS: IP22: Reformat inline assembler code to modern standards.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit 55e4fac7c9ef6e762a64f7e1749f5fb1460948d7)

--- arch/mips/sgi/kernel/indy_sc.c
+++ arch/mips/sgi/kernel/indy_sc.c
@@ -33,30 +33,34 @@ static unsigned long scache_size;
 
 static inline void indy_sc_wipe(unsigned long first, unsigned long last)
 {
-	__asm__ __volatile__("
-		.set	noreorder
-		.set	mips3
-		.set	noat
-		mfc0	$2, $12
-		li	$1, 0x80	# Go 64 bit
-		mtc0	$1, $12
+	unsigned long tmp;
 
-		dli	$1, 0x9000000080000000
-		or	%0, $1		# first line to flush
-		or	%1, $1		# last line to flush
-		.set	at
-
-1:		sw	$0, 0(%0)
-		bne	%0, %1, 1b
-		daddu	%0, 32
-
-		mtc0	$2, $12		# Back to 32 bit
-		nop; nop; nop; nop;
-		.set mips0
-		.set reorder"
-		: /* no output */
-		: "r" (first), "r" (last)
-		: "$1");
+	__asm__ __volatile__(
+	"	.set	push			# indy_sc_wipe		\n"
+	"	.set	noreorder					\n"
+	"	.set	mips3						\n"
+	"	.set	noat						\n"
+	"	mfc0	%2, $12						\n"
+	"	li	$1, 0x80		# Go 64 bit		\n"
+	"	mtc0	$1, $12						\n"
+	"								\n"
+	"	dli	$1, 0x9000000080000000				\n"
+	"	or	%0, $1			# first line to flush	\n"
+	"	or	%1, $1			# last line to flush	\n"
+	"	.set	at						\n"
+	"								\n"
+	"1:	sw	$0, 0(%0)					\n"
+	"	bne	%0, %1, 1b					\n"
+	"	 daddu	%0, 32						\n"
+	"								\n"
+	"	mtc0	%2, $12			# Back to 32 bit	\n"
+	"	nop				# pipeline hazard	\n"
+	"	nop							\n"
+	"	nop							\n"
+	"	nop							\n"
+	"	.set	pop						\n"
+	: "=r" (first), "=r" (last), "=&r" (tmp)
+	: "0" (first), "1" (last));
 }
 
 static void indy_sc_wback_invalidate(unsigned long addr, unsigned long size)
